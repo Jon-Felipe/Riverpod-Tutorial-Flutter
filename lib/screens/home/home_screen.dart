92071a2b592e735cd8d51aaab3f6bd0d965e8c84
Jon-Felipe <rodriguesjf7@gmail.com>
2025-04-23 12:46:31 +0200

    setup and used cart provider notifier

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_tutorial_flutter/providers/cart_provider.dart';
 import 'package:riverpod_tutorial_flutter/providers/products_provider.dart';
 import 'package:riverpod_tutorial_flutter/screens/shared/cart_icon.dart';
 
@@ -9,6 +10,7 @@ class HomeScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final allProducts = ref.watch(productsProvider);
+    final cartProducts = ref.watch(cartNotifierProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -34,6 +36,13 @@ class HomeScreen extends ConsumerWidget {
                   Image.asset(allProducts[index].image, width: 60, height: 60),
                   Text(allProducts[index].title),
                   Text('R${allProducts[index].price}'),
+                  if (cartProducts.contains(allProducts[index]))
+                    TextButton(onPressed: () {}, child: const Text('Remove')),
+                  if (!cartProducts.contains(allProducts[index]))
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('Add to Cart'),
+                    ),
                 ],
               ),
             );
